921fb96e41155db446f18ed8a80d95da510d720a
Студент1 <1@DESKTOP-7RO4MU5>
2022-10-25 12:19:37 +0300

    Добавьте файлы проекта.

---
+++ Regex/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Text.RegularExpressions;
+void Number()
+{
+    Console.WriteLine("Введите номер телефона");
+    string number = Console.ReadLine();
+    Regex regex = new Regex(@"[7|8]{1}-\d{3}-\d{3}-\d{2}-\d{2}$");
+    Regex regex1 = new Regex(@"[7|8]{1} \d{3} \d{3} \d{2} \d{2}$");
+    Regex regex2 = new Regex(@"[7|8]\d{10}$");
+    Regex regex3 = new Regex(@"[7|8]{1} \d{3} \d{3} \d{4}$");
+    Regex regex4 = new Regex(@"[7|8]{1}-\d{3}-\d{3}-\d{4}$");
+    if (Mail() == false)
+    {
+        return;
+    }
+    else if (regex.IsMatch(number))
+    {
+        Console.WriteLine("Молодец");
+    }
+    else if (regex1.IsMatch(number))
+    {
+        Console.WriteLine("Молодец");
+    }
+    else if (regex2.IsMatch(number))
+    {
+        Console.WriteLine("Молодец");
+    }
+    else if (regex3.IsMatch(number))
+    {
+        Console.WriteLine("Молодец");
+    }
+    else if (regex4.IsMatch(number))
+    {
+        Console.WriteLine("Молодец");
+    }
+    else
+    {
+        Console.WriteLine("Некоректный номер");
+    }
+}
+bool Mail()
+{
+    Console.WriteLine("Введите почту");
+    string mail = Console.ReadLine();
+    Regex mail1 = new Regex(@"^[a-z]{1,20}@mail.ru$");
+    if (!mail1.IsMatch(mail))
+    {
+        Console.WriteLine("Неверная почта");
+        return false;
+    }
+    return true;
+}
+Number();
+
+
+
